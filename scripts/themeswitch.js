833084c1fea7e6122ab30d52a8aad0ce67460f75
ian-hall <ian-hall@users.noreply.github.com>
2016-05-13 11:30:30 -0700

    Adding more content

--- scripts/themeswitch.js
+++ scripts/themeswitch.js
@@ -1,38 +1,35 @@
-﻿// *** TO BE CUSTOMISED ***
-
-var style_cookie_name = "style";
+﻿var style_cookie_name = "style";
 var style_cookie_duration = 30;
-var style_domain = "thesitewizard.com";
-
-// *** END OF CUSTOMISABLE SECTION ***
-// You do not need to customise anything below this line
+var style_domain = "";
 
 function switch_style(css_title) {
     // You may use this script on your site free of charge provided
     // you do not remove this notice or the URL below. Script from
     // http://www.thesitewizard.com/javascripts/change-style-sheets.shtml
     var i, link_tag;
-    for (i = 0, link_tag = document.getElementsByTagName("link") ;
-      i < link_tag.length ; i++) {
-        if ((link_tag[i].rel.indexOf("stylesheet") != -1) &&
-          link_tag[i].title) {
+    for (i = 0, link_tag = document.getElementsByTagName("link") ; i < link_tag.length; i++)
+    {
+        if ((link_tag[i].rel.indexOf("stylesheet") != -1) && link_tag[i].title)
+        {
             link_tag[i].disabled = true;
-            if (link_tag[i].title == css_title) {
+            if (link_tag[i].title == css_title)
+            {
                 link_tag[i].disabled = false;
             }
         }
-        set_cookie(style_cookie_name, css_title,
-          style_cookie_duration, style_domain);
+        set_cookie(style_cookie_name, css_title, style_cookie_duration, style_domain);
     }
 }
-function set_style_from_cookie() {
+function set_style_from_cookie()
+{
     var css_title = get_cookie(style_cookie_name);
-    if (css_title.length) {
+    if (css_title.length)
+    {
         switch_style(css_title);
     }
 }
-function set_cookie(cookie_name, cookie_value,
-    lifespan_in_days, valid_domain) {
+function set_cookie(cookie_name, cookie_value, lifespan_in_days, valid_domain)
+{
     // http://www.thesitewizard.com/javascripts/cookies.shtml
     var domain_string = valid_domain ?
                        ("; domain=" + valid_domain) : '';
@@ -42,7 +39,8 @@ function set_cookie(cookie_name, cookie_value,
                        24 * lifespan_in_days +
                        "; path=/" + domain_string;
 }
-function get_cookie(cookie_name) {
+function get_cookie(cookie_name)
+{
     // http://www.thesitewizard.com/javascripts/cookies.shtml
     var cookie_string = document.cookie;
     if (cookie_string.length != 0) {
