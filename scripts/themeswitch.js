e78ff7148be41642962b291c724a789bb2dfcbef
ian-hall <ian-hall@users.noreply.github.com>
2016-07-22 13:39:44 -0700

    maybe eliminated errors caused by the old style cookies

--- scripts/themeswitch.js
+++ scripts/themeswitch.js
@@ -54,10 +54,12 @@ function get_cookie(cookie_name) {
     var cookie_string = document.cookie;
     if (cookie_string.length != 0) {
         var cookie_value = cookie_string.match(
-                        '(^|;)[\s]*' +
                         cookie_name +
                         '=([^;]*)');
-        return decodeURIComponent(cookie_value[2]);
+        if (cookie_value == null) {
+            return "light";
+        }
+        return decodeURIComponent(cookie_value[1]);
     }
     return '';
 }
